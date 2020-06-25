a88dbdf0e9992996156787e285b7864f9dcde24a
Michael Kutzer <kutzer@usna.edu>
2020-06-25 10:02:59 -0400

    Update patchFaceAdjacency.m

--- PatchToolboxFunctions/patchFaceAdjacency.m
+++ PatchToolboxFunctions/patchFaceAdjacency.m
@@ -1,10 +1,70 @@
-function [adj,g] = patchFaceAdjacency(ptch)
-% PATCHFACEADJACENCY returns the adjacency matrix that identifies
+function [adj,faceAdj] = patchFaceAdjacency(ptch)
+% PATCHFACEADJACENCY returns the undirected graph adjacency identifying
 % connections between faces with shared edges. 
 %   adj = PATCHFACEADJACENCY(ptch) defines the patch as either a structured
 %   array containing fields "Faces" and "Vertices" or a patch object.
+%
+%   [___,faceAdj] = PATCHFACEADJACENCY(___) additionally returns an Nx3
+%   array containing face index values.
+%
+%   Inputs:
+%       ptch - patch object or structure
+%           ptch.Faces    - Nx3 array containing vertex indices
+%           ptch.Vertices - Mx3 array containing vertex coordinates
+%
+%   Outputs:
+%       adj - NxN sparse matrix containing a "1" if the corresponding face
+%             indices share an edge
+%       faceAdj - Nx3 array containing the face indices that are adjacent
+%                 to the face defined by the row index
+%           faceAdj(i,:) - the index values of the three faces adjacent to
+%                          face i
 %
 %   NOTE: This function assumes the patch is defined using a triangular
 %   mesh
 %
 %   M. Kutzer, 01Jun2020, USNA
+
+%% Check input(s)
+narginchk(1,1);
+
+try
+    v = ptch.Vertices;
+    if size(v,2) < 3
+        % Make vertices 3D
+        v(:,3) = 0;
+    end
+    f = ptch.Faces;
+    if size(f,2) > 3
+        warning('This function assumes the patch is defined using a triangular mesh.');
+    end
+catch
+    error('Patch must be defined with "Vertices" and "Faces".');
+end
+
+%% Find faces that share vertices
+N = size(f,1);
+m = size(f,2); % This *should* be 3 (if the mesh is triangular)
+
+adj = sparse(N,N);
+fAdj = uint32(zeros(N,m)); % TODO - consider 8-bit, 16-bit, 32-bit switch based on number of faces
+for i = 1:N
+    % Find shared vertices
+    bin = false(N,m);
+    for j = 1:m
+        f_check = repmat(f(i,j),N,m);
+        bin = bin | (f == f_check);
+    end
+    % Sum to find pairs of shared vertices
+    % TODO - This step becomes more complicated for meshes that are not
+    %        triangular
+    sBIN = sum(bin,2);
+    
+    % Define edge adjacencies
+    % -> Two shared vertices on a trianglular mesh - edges are shared.
+    faceAdj(i,:) = reshape( uint32(find(sBIN == 2)),1,[]); % TODO - consider 8-bit, 16-bit, 32-bit switch based on number of faces
+    adj(i,faceAdj(i,:)) = 1;
+end
+
+
+
