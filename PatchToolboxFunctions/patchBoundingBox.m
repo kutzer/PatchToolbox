8c8aa61f273c96fc7b8e5ad5579a20cf42cef8c2
Michael Kutzer <kutzer@usna.edu>
2021-03-16 07:48:28 -0400

    Update patchBoundingBox.m

--- PatchToolboxFunctions/patchBoundingBox.m
+++ PatchToolboxFunctions/patchBoundingBox.m
@@ -1,12 +1,28 @@
-function p = patchBoundingBox(X_lims)
-% PATCHBOUNDINGBOX visualizes the bounding box for a given set of limits
-%   p = PATCHBOUNDINGBOX(X_lims) visualizes the bounding box described by
-%   X_lims.
+function varargout = patchBoundingBox(varargin)
+% PATCHBOUNDINGBOX defines face and vertex information for a bounding box 
+% given set of limits OR defines a bounding box given one or more patch
+% objects.
+%   p = PATCHBOUNDINGBOX(X_lims) defines face and vertex information for a 
+%   bounding box given a set of limits described by X_lims.
 %
 %   Inputs:
 %       X_lims - 3x2 array containing x/y/z limits
 %   Outputs:
 %       p - structured array containing patch fields
+%
+%   [X_lims, X_lim_i] = PATCHBOUNDINGBOX(p) calculates the overall bounding
+%   box for one or more patch objects (X_lims), and calculates the
+%   individual bounding box for each element of p (X_lim_i).
+%
+%   Inputs:
+%       p - n-element structured array containing fields "Faces" and
+%           "Vertices" or a valid array of patch objects.
+%
+%   Outputs:
+%       X_lims  - 3x2 array containing x/y/z limits for all patch objects
+%       X_lim_i - n-element cell array
+%           X_lim_i{i} - 3x2 array containing x/y/z limits for the ith
+%                        patch object
 %
 %   M. Kutzer & L. Davis, 04Feb2021, USNA
 
@@ -22,6 +38,41 @@ end
 
 %% Check input(s)
 narginchk(1,1);
+switch class(varargin{1})
+    case 'struct'
+        p = varargin{1};
+        X_lims = [];
+        fields = {'Faces','Vertices'};
+        bin = isfield(p,fields);
+        if nnz(bin) ~= numel(fields)
+            error('Input must be a struct with fields "Faces" and "Vertices" or a valid patch object');
+        end
+    case 'matlab.graphics.primitive.Patch'
+        p = varargin{1};
+        X_lims = [];
+    otherwise
+        X_lims = varargin{1};
+end
+ 
+%% Define X_lims (if patch is provided)
+if isempty(X_lims)
+    for i = 1:numel(p)
+        v = p(i).Vertices;
+        for j = 1:size(v,2)
+            X_lim_i{i}(j,:) = [min(v(:,j)),max(v(:,j))];
+        end
+        
+        X_lims = [X_lims, X_lim_i{i}];
+    end
+    X_lims = [min(X_lims,[],2),max(X_lims,[],2)];
+    
+    % Package outputs
+    varargout{1} = X_lims;
+    varargout{2} = X_lim_i;
+    return
+end
+
+%% Run remainder of code
 if size(X_lims,1) ~= 3 || size(X_lims,2) ~= 2
     error('The input must be a 3x2 array containing x/y/z limits.');
 end
@@ -80,4 +131,6 @@ end
 %% Package patch struct
 p.Vertices = v;
 p.Faces = f;
+
+varargout{1} = p;
     
