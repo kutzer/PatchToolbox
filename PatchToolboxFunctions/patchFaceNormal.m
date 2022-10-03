e0ee0a07ec220863c0d44dd4e96a6e84f65cda43
Michael Kutzer <kutzer@usna.edu>
2022-10-03 11:36:49 -0400

    Update patchFaceNormal.m

--- PatchToolboxFunctions/patchFaceNormal.m
+++ PatchToolboxFunctions/patchFaceNormal.m
@@ -10,13 +10,38 @@ function [n_hat,n] = patchFaceNormal(ptc,idx)
 %
 %   [n_hat,n] = PATCHFACENORMAL(___) also returns the normal.
 %
+%   Input(s)
+%       ptc - patch object or structured array with fields 'Vertices' and
+%            'Faces'
+%           ptc.Vertices - Nx3 array containing 3D vertex coordinates
+%           ptc.Faces    - Mx3 array containing vertex indices describing
+%                          triangular faces
+%       idx - [OPTIONAL] face index or indices for calculating the normal.
+%             If idx is not specified, normals are calculated for all
+%             faces.
+%
+%   Output(s)
+%       n_hat - Kx3 array defining the unit normal for each face specified
+%               in idx, or all faces if idx is not specified (i.e. K = M).
+%       n     - Kx3 array defining the normal for each face
+%
 %   NOTE: This function assumes the patch is defined using a triangular
 %   mesh
 %
 %   M. Kutzer, 12Jun2020, USNA
 
+% Update(s)
+%   03Oct2022 - Updated documentation
+
 %% Check/parse inputs
 narginchk(1,2);
+
+try
+    v = ptch.Vertices;
+    f = ptch.Faces;
+catch
+    error('Patch must be defined with "Vertices" and "Faces".');
+end
 
 if nargin < 2
     M = size(ptc.Faces,1);
