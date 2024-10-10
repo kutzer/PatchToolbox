f03ea33aa47a659733e79b991bc47bb4f49ad36b
Michael Kutzer <kutzer@usna.edu>
2024-10-10 16:37:41 -0400

    Update patchEdgeInfo.m

--- PatchToolboxFunctions/patchEdgeInfo.m
+++ PatchToolboxFunctions/patchEdgeInfo.m
@@ -86,8 +86,8 @@ h = waitbar(0,'Mapping unique edges to vertex indices...','Name','patchEdgeInfo.
 m = size(v,1);          % Total number of vertices
 for vertex = 1:m
     bin = any(uEdges == vertex,2);
-    fprintf('%6d ',find(bin'));
-    fprintf('\n');
+    %fprintf('%6d ',find(bin'));
+    %fprintf('\n');
     eVertices(vertex,:) = reshape(find(bin),1,[]);
     waitbar(vertex/m,h);
 end
