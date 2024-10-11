0375c1e8a0027803851047b2d7e42f4602d62227
Michael Kutzer <kutzer@usna.edu>
2024-10-11 13:45:26 -0400

    Update findUndirectedGraphCycles.m

--- PatchToolboxFunctions/findUndirectedGraphCycles.m
+++ PatchToolboxFunctions/findUndirectedGraphCycles.m
@@ -1,7 +1,31 @@
 function cycles = findUndirectedGraphCycles(G)
-% Input: G is the adjacency matrix of an undirected graph
-% Output: cycles is a cell array containing the largest set of non-overlapping cycles
+% FINDUNDIRECTEDGRAPHCYCLES finds the largest set of non-overlapping cycles
+% in a directed graph. 
+%   cycles = findUndirectedGraphCycles(G)
+%
+%   Input(s)
+%       G - undirected graph object defining directional connectivity 
+%           between nodes
+%
+%   Output(s)
+%          cycles - N-element cell array containing ordered node indices
+%                   defining closed cycles in the directedGraph
+%
+%   See also graph findDirectedGraphCycles
+%
+%   M. Kutzer, 13Sep2024, USNA
 
+%% Check input(s)
+narginchk(1,1);
+
+switch lower( class(directedGraph) )
+    case 'graph'
+        % Good input
+    otherwise
+        error('Graph input must be specified as an undirected graph.');
+end
+
+%% Find cycles
 numNodes = size(G, 1);
 allCycles = {};
 visited = false(1, numNodes);
